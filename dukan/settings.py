7c4dd4261a5528cc7d67f2d4b47b4c78666a4293
Ak@sh <aaj7731@gmail.com>
2021-09-02 03:42:17 -0700

    first commit

--- dukan/settings.py
+++ dukan/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os 
 import django_heroku
 import dj_database_url
-from decouple import confi
+from decouple import Config
 
 from pathlib import Path
 
